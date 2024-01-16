fa8af309ec8936b3b0db09b268b5a616f49ac1a5
Monica Tuttle <monica.tuttle16@gmail.com>
2024-01-15 22:32:10 -0500

    Fixed up the Contact page

--- portfolio/src/app/contact/page.js
+++ portfolio/src/app/contact/page.js
@@ -4,46 +4,43 @@ import React from "react";
 
 function Contact() {
   return (
-    <div className="bg-green-50">
-      <main className="bg-green-50 grid grid-flow-col w-3/4">
-        <Menu />
-        <div>
-          <h2 className="text-center mt-20 mb-10 text-lg">Contact Me</h2>
+    <div>
+      <Menu />
+      <main className="bg-green-50 h-screen">
+        <form className="m-auto md:ml-80 md:pr-32 w-3/4 grid text-lg">
+          <h2 className="text-center pt-24 md:pt-20 text-lg">Contact Me</h2>
+          <label htmlFor="name">Name:</label>
+          <input
+            className="bg-green-200 p-2 rounded md text-lg mt-3 mb-3"
+            type="text"
+            id="name"
+            name="name"
+            required
+          />
+          <label htmlFor="email">Email:</label>
+          <input
+            className="bg-green-200 p-2 rounded md text-lg mt-3 mb-3"
+            type="email"
+            id="email"
+            name="email"
+            required
+          />
 
-          <form className="grid grid-cols-1 mt-4 text-lg">
-            <label htmlFor="name">Name:</label>
-            <input
-              className="bg-green-200 p-2 rounded md text-lg mt-3 mb-3"
-              type="text"
-              id="name"
-              name="name"
-              required
-            />
-            <label htmlFor="email">Email:</label>
-            <input
-              className="bg-green-200 p-2 rounded md text-lg mt-3 mb-3"
-              type="email"
-              id="email"
-              name="email"
-              required
-            />
-
-            <label htmlFor="message">Message:</label>
-            <textarea
-              className="bg-green-200 p-2 rounded md text-lg mt-3 mb-3"
-              id="message"
-              name="message"
-              rows="5"
-              required
-            ></textarea>
-              <button
-                className="bg-purple-500 p-4 hover:bg-purple-200 cursor-pointer rounded md text-lg mt-10 mb-3"
-                type="submit"
-              >
-                Submit
-              </button>
-          </form>
-        </div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            className="bg-green-200 p-2 rounded md text-lg mt-3 mb-3"
+            id="message"
+            name="message"
+            rows="5"
+            required
+          ></textarea>
+          <button
+            className="bg-purple-500 p-4 hover:bg-purple-200 cursor-pointer rounded md text-lg mt-10 mb-3"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
       </main>
     </div>
   );
