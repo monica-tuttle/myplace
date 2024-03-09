7417ad7c2e8fe7abe1a73591c7ca76b097239d79
Monica Tuttle <monica.tuttle16@gmail.com>
2024-03-09 18:00:54 -0500

    Reverted back to old imports

--- portfolio/src/app/api/add-contact/route.js
+++ portfolio/src/app/api/add-contact/route.js
@@ -1,13 +1,13 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import { sql } from '@vercel/postgres';
+import { NextResponse } from 'next/server';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method Not Allowed' });
+export default async function handler(request) {
+  if (request.method !== 'POST') {
+    return NextResponse.error(new Error('Method Not Allowed'), { status: 405 });
   }
 
   try {
-    const { name, email, message } = req.body; // Extract fields from request body
+    const { name, email, message } = await request.json(); // Extract fields from request body
 
     if (!name || !email || !message) {
       throw new Error('Name, email, and message are required');
@@ -15,9 +15,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // Inserting data into the database
     await sql`INSERT INTO contact_form (Name, Email, Message) VALUES (${name}, ${email}, ${message})`;
 
-    return res.status(200).json({ message: 'Contact added successfully' });
+    return NextResponse.json({ message: 'Contact added successfully' }, { status: 200 });
   } catch (error) {
     console.error('Error adding contact:', error);
-    return res.status(500).json({ error: 'Internal Server Error' });
+    return NextResponse.error(new Error('Internal Server Error'), { status: 500 });
   }
 }
