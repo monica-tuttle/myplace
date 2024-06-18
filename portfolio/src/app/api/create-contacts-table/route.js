380f005188577ef13a067e52ea32c92fd7f54e33
Monica Tuttle <monica.tuttle16@gmail.com>
2024-06-17 20:49:42 -0400

    Updated create-contacts-table

--- portfolio/src/app/api/create-contacts-table/route.js
+++ portfolio/src/app/api/create-contacts-table/route.js
@@ -1,17 +1,17 @@
-import { sql, db } from '@vercel/postgres';
-import { NextResponse } from 'next/server';
+import { sql, db } from "@vercel/postgres";
+import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
-    const client = await db.connect(); // Moved inside the async function
+    const client = await db.connect();
     const result = await sql`CREATE TABLE IF NOT EXISTS contact_form (
-          id SERIAL PRIMARY KEY,
-          full_name VARCHAR(100) NOT NULL,
-          email VARCHAR(100) NOT NULL,
-          message TEXT NOT NULL,
-          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-      );`;
-    client.release(); // Release the client connection
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(100) NOT NULL,
+      email VARCHAR(100) NOT NULL,
+      message TEXT NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );`;
+    client.release();
     return NextResponse.json({ result }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
